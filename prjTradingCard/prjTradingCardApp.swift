6125f80812c8f5782731b6e1e7b807072a9be949
CongHuyVHS <conghuykieu2005@gmail.com>
2025-11-02 10:35:42 -0500

    Initial Commit

---
+++ prjTradingCard/prjTradingCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  prjTradingCardApp.swift
+//  prjTradingCard
+//
+//  Created by Danh on 2025-11-02.
+//
+
+import SwiftUI
+
+@main
+struct prjTradingCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
