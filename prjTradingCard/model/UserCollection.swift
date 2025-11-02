2b2953c1569499c8d450d159137bacafec17adad
CongHuyVHS <conghuykieu2005@gmail.com>
2025-11-02 11:58:59 -0500

    implemented model: user, card, userCollection and userNotification

---
+++ prjTradingCard/model/UserCollection.swift
@@ -0,0 +1,14 @@
+//
+//  UserCollection.swift
+//  prjTradingCard
+//
+//  Created by Danh on 2025-11-02.
+//
+
+import Foundation
+
+struct UserCollection{
+    let userId: Int
+    let cardId: Int
+    
+}
